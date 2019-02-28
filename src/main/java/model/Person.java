8ac1027076e8769317c2adf66a39cb40076814fd
15279676722 <1085326293@qq.com>
2019-02-28 16:00:45 +0800

    第二次提交

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -4,11 +4,11 @@ public class Person {
     private Integer id;
     private String username;
     private int age;
-
+    private int ss;
     public String getUsername() {
         return username;
     }
-
+   
     public void setUsername(String username) {
         this.username = username;
     }
